07cbde84e2ea3a30ad8e7e0c39e697478eca7276
Nathan Fletcher <Njfletcher215@gmail.com>
2023-03-21 14:06:09 -0500

    switched from react logo to placeholder image

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -6,7 +6,7 @@ export default function Landing() {
   return (
     <div className="page-content-container">
       <div className="center-text-content">
-        <img src='/logo192.png' />
+        <img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAACkUlEQVR4Xu2Y14piQRCGy5wTBswYMKGI4Pu/gVcGEBX1RhAjKua0/L2MKOswo3s8CFbDAfV0d3X9FT5sRalUOtMHDwULwBnAJcA94IN7IHETZAowBZgCTAGmwAcrwBhkDDIGGYOMwQ+GAP8ZYgwyBhmDjEHGIGPwQQWUSiX5fD4ym810Op1oMBjQbDa72cVms1EgEKBOp0Pr9fpBC3+ny2HnKQzG43Gy2+20WCxIo9GQXq+nWq1Gm81GHFytVlMulyOVSkWNRkPMe2bIYedhAeBsNpulfr9PvV5PRCkajdJwOKT5fC78TCaTZLFYxOd7AqRSKVIoFOIdBr7v93tqtVoXnaSw8xvRHxbA4XBQLBYTaQ3nd7sdjUYjmkwmwh5KAw/ECQaDdwXweDwUCoXEOgjhdDqp3W7TdDq9nFkKOy8RwOv1ito+n88i4ugDSPVKpSLKAdFEdiDtkQnflQDmYS0GxEOvuB5S2flJhIczANH1+/0iwnAUTufzefEZjc9gMIh3Op2OXC6XiDLKY7Va3ZwFzkMEjHK5LErgekhlR3IBkK6RSERE7Cvti8WicNJkMpFWqxU2UR54QAkIAlJcj0QiQVarVfw0Ho+p2+3evJfKjuQCoMMj4uj4cApOoF7r9Totl8uLPYiRTqfvloDb7aZwOCzWo9lhj2azeWmi2EQKOz85j/cPlwAWwQE0MTSw72rYaDRSJpP5RxjMLxQKdDweL30DyEQJVKvVmzP/r52XCfC1MaK83W7pcDj8xtbTc15p56kMeNqTN1zIAvCFCF+I8IUIX4i8YXOW7UhMAaYAU4ApwBSQreW+oSGmAFOAKcAUYAq8YXOW7UhMAaYAU4ApwBSQreW+oSGmAFPgwynwB1WlXB8xfh2IAAAAAElFTkSuQmCC' />
         <div className="force-break" />
         <a className="centered" href="/#/home">Enter</a>
       </div>
