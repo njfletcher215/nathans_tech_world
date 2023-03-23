358de9b29045a012b8b2f48d91a0269e8c2da997
Nathan Fletcher <Njfletcher215@gmail.com>
2023-03-22 19:45:22 -0500

    added simple cookies

--- src/App.js
+++ src/App.js
@@ -13,25 +13,35 @@ import Schematics from './pages/Schematics';
 import CoolTech from './pages/CoolTech';
 import Games from './pages/Games';
 import LogBook from './pages/LogBook';
+import Article from './pages/Article';
+import { CookiesProvider } from 'react-cookie';
+import { useCookies } from 'react-cookie';
 
 function App() {
+  const [cookies, setCookie] = useCookies(['visitCount']);
+
+  setCookie('visitCount', 0, {path: '/#/home'});
+
   return (
     <div className="page-container">
-      <Header />
-      <div className="page-content-container">
-        <Router>
-          <Routes>
-              <Route exact path='/' element={<Landing />} />
-              <Route path='/home' element={<Home />} />
-              <Route path='/software-tutorials' element={<SoftwareTutorials />} />
-              <Route path='/hardware-tutorials' element={<HardwareTutorials />} />
-              <Route path='/schematics' element={<Schematics />} />
-              <Route path='/cool-tech' element={<CoolTech />} />
-              <Route path='/games' element={<Games />} />
-              <Route path='/log-book' element={<LogBook />} />
-          </Routes>
-        </Router>
-      </div>
+      <CookiesProvider>
+        <Header />
+        <div className="page-content-container">
+          <Router>
+            <Routes>
+                <Route exact path='/' element={<Landing />} />
+                <Route path='/home' element={<Home />} />
+                <Route path='/software-tutorials' element={<SoftwareTutorials />} />
+                <Route path='/hardware-tutorials' element={<HardwareTutorials />} />
+                <Route path='/schematics' element={<Schematics />} />
+                <Route path='/cool-tech' element={<CoolTech />} />
+                <Route path='/games' element={<Games />} />
+                <Route path='/log-book' element={<LogBook />} />
+                <Route path='/article/:id' element={<Article />} />
+            </Routes>
+          </Router>
+        </div>
+      </CookiesProvider>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,32 +20,32 @@ export default function Navbar() {
         <li className='nav-item' onMouseEnter={(event) => showTargetClass(event, 'hardware-tutorials-sub-nav')} onMouseLeave={(event) => hideTargetClass(event, 'hardware-tutorials-sub-nav')}>
           <a className='nav-link' href="/#/hardware-tutorials">Hardware Tutorials</a>
           <SubNav className="hardware-tutorials-sub-nav" links={[
-                {url: "/#/article/0", name: "Article 0"},
-                {url: "/#/article/1", name: "Article 1"},
-                {url: "/#/article/2", name: "Article 2"},
-                {url: "/#/article/3", name: "Article 3"}
+                {url: "/#/article/5", name: "Article 5"},
+                {url: "/#/article/6", name: "Article 6"},
+                {url: "/#/article/7", name: "Article 7"},
+                {url: "/#/article/8", name: "Article 8"}
             ]} />
         </li>
         <li className='nav-item' onMouseEnter={(event) => showTargetClass(event, 'schematics-sub-nav')} onMouseLeave={(event) => hideTargetClass(event, 'schematics-sub-nav')}>
           <a className='nav-link' href="/#/schematics">Schematics</a>
           <SubNav className="schematics-sub-nav" links={[
-                {url: "/#/article/0", name: "Schematic 0"},
-                {url: "/#/article/1", name: "Schematic 1"},
-                {url: "/#/article/2", name: "Schematic 2"}
+                {url: "/#/article/9", name: "Article 9"},
+                {url: "/#/article/10", name: "Article 10"},
+                {url: "/#/article/11", name: "Article 11"}
             ]} />
         </li>
         <li className='nav-item' onMouseEnter={(event) => showTargetClass(event, 'cool-tech-sub-nav')} onMouseLeave={(event) => hideTargetClass(event, 'cool-tech-sub-nav')}>
           <a className='nav-link' href="/#/cool-tech">Cool Tech!</a>
           <SubNav className="cool-tech-sub-nav" links={[
-                {url: "/#/article/0", name: "Article 0"},
-                {url: "/#/article/1", name: "Article 1"}
+                {url: "/#/article/12", name: "Article 12"},
+                {url: "/#/article/13", name: "Article 13"}
             ]} />
         </li>
         <li className='nav-item' onMouseEnter={(event) => showTargetClass(event, 'games-sub-nav')} onMouseLeave={(event) => hideTargetClass(event, 'games-sub-nav')}>
           <a className='nav-link' href="/#/games">Games</a>
           <SubNav className="games-sub-nav" links={[
-                {url: "/#/article/0", name: "Game 0"},
-                {url: "/#/article/1", name: "Game 1"}
+                {url: "/#/article/14", name: "Article 14"},
+                {url: "/#/article/15", name: "Article 15"}
             ]} />
         </li>
       </ul>
